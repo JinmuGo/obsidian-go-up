86cc6f9d280b245f4b332340bf293ae35b1b7548
JinMu Go <jingmuio@gmail.com>
2024-03-07 21:01:56 +0900

    fix/work with aliases (#2)
    * chore: add package-lock.json to .gitignore
    
    * refactor: define upProperty local var
    
    * fix: work with aliases

--- src/goMultiPage.ts
+++ src/goMultiPage.ts
@@ -1,18 +1,28 @@
 import { MultiPageModal } from "./modal/MultiPageModal";
 import { goPageType } from "./types/goPage";
+import checkAlias from "./utils/checkAlias";
 import getPageName from "./utils/getPageName";
 import type { App } from "obsidian";
+import getPageNameInAlias from "./utils/getPageNameInAlias";
 
 const goMultiPage = (upPages: Array<string>, goPage: goPageType, app: App) => {
 	upPages = upPages.reduce((prev: string[], upPage: string) => {
-		const pageName = getPageName(upPage);
-		if (pageName === null) return prev;
+		let pageName = getPageName(upPage);
+		if (pageName === null) {
+			return prev;
+		}
+		if (checkAlias(pageName)) {
+			pageName = getPageNameInAlias(pageName);
+		}
 		prev.push(pageName);
 		return prev;
 	}, []);
 
-	if (upPages.length === 1) goPage(upPages[0], "", false);
-	else new MultiPageModal(app, goPage, upPages).open();
+	if (upPages.length === 1) {
+		goPage(upPages[0], "", false);
+	} else {
+		new MultiPageModal(app, goPage, upPages).open();
+	}
 };
 
 export default goMultiPage;
